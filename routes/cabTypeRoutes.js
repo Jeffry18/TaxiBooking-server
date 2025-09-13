acc52a5d15bb756e0d524acb86e10eac152a3662
Jeffry18 <jeffryrony14@gmail.com>
2025-09-13 12:23:18 +0530

    next13

--- routes/cabTypeRoutes.js
+++ routes/cabTypeRoutes.js
@@ -1,8 +1,23 @@
 const express = require("express");
-const upload = require("../middlewares/multterMiddleware");
+const multer = require("multer")
+const path = require("path")
+const cabTpye = require("../models/cabType")
 const { getCabTypes, createCabType, deleteCabType } = require("../controllers/cabTypeController");
-
 const router = express.Router();
+
+// Multer storage configuration for package images
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "../uploads"));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+  });
+  
+  const upload = multer({ storage });
+
 
 router.get("/", getCabTypes);
 router.post("/", upload.single("image"), createCabType);
