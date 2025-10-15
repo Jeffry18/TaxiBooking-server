1fe508a9615bc825462651aa547734f6d188490a
Jeffry18 <jeffryrony14@gmail.com>
2025-10-15 10:03:26 +0530

    next51

--- models/cabType.js
+++ models/cabType.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cabTypeSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
-  seats: { type: Number},
+  seats: { type: String},
   image: { type: String }, // store filename or URL
 }, { timestamps: true });
 
